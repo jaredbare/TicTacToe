644efaef03fe8429fb0681036c748fcc04c48f39
jaredbare <jab468@byu.edu>
2023-01-23 15:32:50 -0700

    print

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,41 +7,41 @@ namespace TicTacToe
 
     public class Driver
     {
-        static char[] board;
+        static char[] choiceArray;
 
         static void Main(string[] args)
         {
             // Welcome the user to the game
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
-            // Create a game board array to store the players' choices
-            board = new char[9];
+            // Create a game choiceArray array to store the players' choices
+            choiceArray = new char[9];
 
-            // Ask each player in turn for their choice and update the game board array
-            for (int i = 0; i < board.Length; i++)
+            // Ask each player in turn for their choice and update the game choiceArray array
+            for (int i = 0; i < choiceArray.Length; i++)
             {
                 Console.Write($"Player {((i % 2) + 1)}: Enter your choice: ");
-                board[i] = Console.ReadLine()[0];
-                // Print the board by calling the method in the supporting class
-                PrintBoard();
+                choiceArray[i] = Console.ReadLine()[0];
+                // Print the choiceArray by calling the method in the supporting class
+                PrintchoiceArray();
             }
 
             // Check for a winner by calling the method in the supporting class
             // and notify the players when a win has occurred and which player won the game
-            int winner = SupportingClass.CheckForWinner(board);
+            int winner = SupportingClass.CheckForWinner(choiceArray);
             if (winner > 0)
                 Console.WriteLine($"Player {winner} has won the game!");
             else
                 Console.WriteLine("It's a draw!");
         }
 
-        static void PrintBoard()
+        static void PrintchoiceArray()
         {
-            Console.WriteLine($" {board[0]} | {board[1]} | {board[2]}");
+            Console.WriteLine($" {choiceArray[0]} | {choiceArray[1]} | {choiceArray[2]}");
             Console.WriteLine("-----------");
-            Console.WriteLine($" {board[3]} | {board[4]} | {board[5]}");
+            Console.WriteLine($" {choiceArray[3]} | {choiceArray[4]} | {choiceArray[5]}");
             Console.WriteLine("-----------");
-            Console.WriteLine($" {board[6]} | {board[7]} | {board[8]}");
+            Console.WriteLine($" {choiceArray[6]} | {choiceArray[7]} | {choiceArray[8]}");
         }
     }
 
@@ -49,33 +49,41 @@ namespace TicTacToe
 
     public class SupportingClass
     {
-        // Receive the “board” array from the driver class
-        public static int CheckForWinner(char[] board)
+        // Receive the “choiceArray” array from the driver class
+        public static bool CheckForWinner(char[] choiceArray)
         {
             // Horizontal wins
-            if (board[0] == board[1] && board[1] == board[2])
-                return (board[0] == 'X') ? 1 : 2;
-            if (board[3] == board[4] && board[4] == board[5])
-                return (board[3] == 'X') ? 1 : 2;
-            if (board[6] == board[7] && board[7] == board[8])
-                return (board[6] == 'X') ? 1 : 2;
+            bool winner = false;
+            if (choiceArray[0] == choiceArray[1] && choiceArray[1] == choiceArray[2])
+                winner = true;
+            if (choiceArray[3] == choiceArray[4] && choiceArray[4] == choiceArray[5])
+                winner = true;
+            if (choiceArray[6] == choiceArray[7] && choiceArray[7] == choiceArray[8])
+                winner = true;
 
             // Vertical wins
-            if (board[0] == board[3] && board[3] == board[6])
-                return (board[0] == 'X') ? 1 : 2;
-            if (board[1] == board[4] && board[4] == board[7])
-                return (board[1] == 'X') ? 1 : 2;
-            if (board[2] == board[5] && board[5] == board[8])
-                return (board[2] == 'X') ? 1 : 2;
+            if (choiceArray[0] == choiceArray[3] && choiceArray[3] == choiceArray[6])
+                winner = true;
+            if (choiceArray[1] == choiceArray[4] && choiceArray[4] == choiceArray[7])
+                winner = true;
+            if (choiceArray[2] == choiceArray[5] && choiceArray[5] == choiceArray[8])
+                winner = true;
 
             // Diagonal wins
-            if (board[0] == board[4] && board[4] == board[8])
-                return (board[0] == 'X') ? 1 : 2;
-            if (board[2] == board[4] && board[4] == board[6])
-                return (board[2] == 'X') ? 1 : 2;
+            if (choiceArray[0] == choiceArray[4] && choiceArray[4] == choiceArray[8])
+                winner = true;
+            if (choiceArray[2] == choiceArray[4] && choiceArray[4] == choiceArray[6])
+                winner = true;
 
-            // No win
-            return 0;
+            return winner;
+        }
+        static void PrintchoiceArray(char[] choiceArray)
+        {
+            Console.WriteLine($" {choiceArray[0]} | {choiceArray[1]} | {choiceArray[2]}");
+            Console.WriteLine("-----------");
+            Console.WriteLine($" {choiceArray[3]} | {choiceArray[4]} | {choiceArray[5]}");
+            Console.WriteLine("-----------");
+            Console.WriteLine($" {choiceArray[6]} | {choiceArray[7]} | {choiceArray[8]}");
         }
     }
 }
